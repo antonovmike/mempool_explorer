c82da1f74b2bbaf82c93f4a59116abf10db2e1b0
antonovmike <antonovma@outlook.com>
2023-07-05 11:26:45 +0400

    rw mempool

--- src/main.rs
+++ src/main.rs
@@ -19,27 +19,13 @@ enum MyError {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Record {
-    id: i32,
-    name: String,
-    age: i32,
+    id: String,
+    tx: Vec<u8>,
 }
 
 fn main() -> Result<(), MyError> {
-    let connection = Connection::open("add/test.db")?;
-    // let connection = Connection::open("add/mempool.sqlite")?;
-    
-    connection.execute(
-        "CREATE TABLE IF NOT EXISTS people (
-                id              INTEGER PRIMARY KEY,
-                name            TEXT NOT NULL,
-                age             INTEGER NOT NULL
-                );",
-        [],
-    )?;
-
-    // connection.execute(&format!("INSERT INTO people VALUES (0, 'Alice', 42);"), [])?;
-
-    let mut last_id = 0;
+    let connection = Connection::open("add/mempool.sqlite")?;
+    let mut last_id = String::new();
 
     let mut file = OpenOptions::new()
         .write(true)
@@ -49,37 +35,26 @@ fn main() -> Result<(), MyError> {
         .open("add/output.json")
         .unwrap();
 
-    mempool().unwrap();
-
     loop {
-        let mut stmt = connection.prepare("SELECT * FROM people WHERE id > ?")?;
+        let mut stmt = connection.prepare("SELECT * FROM mempool WHERE tx > ?")?;
         let rows = stmt.query_map([&last_id], |row| {
             Ok(Record {
                 id: row.get(0)?,
-                name: row.get(1)?,
-                age: row.get(2)?,
+                tx: row.get(11)?,
             })
         })?;
 
         for row in rows {
             let record = row?;
-            let serialized = serde_json::to_string(&record)?;
+            let serialized = serde_json::to_string(&record.tx)?;
 
             file.write_all(serialized.as_bytes())?;
             file.write_all(b"\n")?;
 
             last_id = record.id;
-            println!("{last_id}, {}", record.name);
+            println!("{:?}", record.tx)
         }
 
         thread::sleep(Duration::from_secs(1));
     }
-}
-
-fn mempool() -> Result<()> {
-    let conn = Connection::open("add/mempool.sqlite")?;
-    let mut stmt = conn.prepare("SELECT tx FROM mempool LIMIT 1")?;
-    let tx: Vec<u8> = stmt.query_row([], |row| row.get(0))?;
-    println!("{:?}", tx);
-    Ok(())
 }
