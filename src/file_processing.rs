6bbdbdb1a1abace0476332ed3b5e5aae15d8eda0
antonovmike <antonovma@outlook.com>
2023-07-13 15:34:43 +0400

    NO_NAME.json if name is empty

--- src/file_processing.rs
+++ src/file_processing.rs
@@ -4,10 +4,7 @@ use std::{
     path::Path,
 };
 
-use blockstack_lib::{
-    chainstate::stacks::StacksTransaction,
-    core::mempool::MemPoolTxInfo,
-};
+use blockstack_lib::{chainstate::stacks::StacksTransaction, core::mempool::MemPoolTxInfo};
 use rusqlite::Result;
 
 use crate::error_handler::MyError;
@@ -54,41 +51,56 @@ pub fn separate_files(transactions: &[StacksTransaction], smart_contract: SmartC
 
     for tx in transactions.iter() {
         let name = smart_contract.contract_name();
-        if !name.is_empty() {
-            if let Some((_, ref mut txs)) =
-                named_transactions.iter_mut().find(|(n, _)| n == &name)
-            {
-                txs.push(tx.clone());
-            } else {
-                named_transactions.push((name.clone(), vec![tx.clone()]));
-            }
+        
+        #[allow(unused_assignments)]
+        let mut name_2 = String::new();
+
+        if name.is_empty() {
+            name_2 = "add/files/NO_NAME.json".to_string()
+        } else {
+            name_2 = format!("add/files/{}.json", name);
+        }
+
+        println!("\t\t{name_2}");
+
+        if let Some((_, ref mut txs)) = named_transactions.iter_mut().find(|(n, _)| n == &name_2) {
+            txs.push(tx.clone());
+        } else {
+            named_transactions.push((name_2.clone(), vec![tx.clone()]));
         }
     }
 
     for (name, txs) in named_transactions.iter() {
-        let name_2 = format!("add/files/{}.json", name);
-        println!("\t\t{name_2}");
-
-        let path = Path::new(&name_2);
+        let path = Path::new(&name);
         if path.exists() {
             let mut temporary_vector: Vec<StacksTransaction> = {
                 File::open(path)
                     .map_err(Into::<MyError>::into)
                     .and_then(|file| serde_json::from_reader(file).map_err(Into::into))
                     .unwrap_or_else(|err| {
-                        log::warn!("failed to load transactions: {err}\nFile '{name_2}' will be recreated");
+                        log::warn!(
+                            "failed to load transactions: {err}\nFile '{name}' will be recreated"
+                        );
                         vec![]
                     })
             };
             temporary_vector.extend_from_slice(txs.as_slice());
-            File::create(path).and_then(|mut f| f.write_all(serde_json::to_string_pretty(&temporary_vector).unwrap().as_bytes())).unwrap();
+            File::create(path)
+                .and_then(|mut f| {
+                    f.write_all(
+                        serde_json::to_string_pretty(&temporary_vector)
+                            .unwrap()
+                            .as_bytes(),
+                    )
+                })
+                .unwrap();
         } else {
             File::create(path).unwrap();
             for tx in txs.iter() {
                 let sc = SmartContract {
                     name_of_smart_contract: smart_contract.name_of_smart_contract.clone(),
                     tx_info_tx: tx.clone(),
-                    filename: name_2.clone(),
+                    filename: name.clone(),
                 };
                 sc.append_data().unwrap();
             }
